d7bfeb368ba5fd29f99447e659266b552f258887
Muzammil96313 <muzammil.hassan.dev7@gmail.com>
2025-01-08 14:32:13 +0500

    new

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -7,7 +7,11 @@ const Portfolio = () => {
         <h1 className="text-4xl font-bold mb-8">Portfolio</h1>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
           <div>
-            <img className="rounded-lg" src="blog.webp" alt="" />
+            <img
+              className="rounded-lg h-[400px] max-h-full"
+              src="blog.webp"
+              alt=""
+            />
             <div className="flex justify-between items-center">
               <h4 className="text-lg font-semibold">Blog App</h4>
               <a href="https://blog-murex-ten-84.vercel.app/" target="_blank">
@@ -16,19 +20,27 @@ const Portfolio = () => {
             </div>
           </div>
           <div>
-            <img className="rounded-lg" src="computer.webp" alt="" />
+            <img
+              className="rounded-lg h-[400px] max-h-full bg-contain"
+              src="pokemon.jpg"
+              alt=""
+            />
             <div className="flex justify-between items-center">
-              <h4 className="text-lg font-semibold">E-Commerce Website</h4>
-              <a href="https://blog-murex-ten-84.vercel.app/" target="_blank">
+              <h4 className="text-lg font-semibold">Pokemon Website</h4>
+              <a href="https://muzammil-pokemon.netlify.app/" target="_blank">
                 <button className="btn">Preview</button>
               </a>
             </div>
           </div>
           <div>
-            <img className="rounded-lg" src="computer.webp" alt="" />
+            <img
+              className="rounded-lg h-[400px] max-h-full"
+              src="computer.webp"
+              alt=""
+            />
             <div className="flex justify-between items-center">
-              <h4 className="text-lg font-semibold">E-Commerce Website</h4>
-              <a href="https://blog-murex-ten-84.vercel.app/" target="_blank">
+              <h4 className="text-lg font-semibold">Chat Gpt3 Clone Website</h4>
+              <a href="https://gpt3clone3.netlify.app/" target="_blank">
                 <button className="btn">Preview</button>
               </a>
             </div>
