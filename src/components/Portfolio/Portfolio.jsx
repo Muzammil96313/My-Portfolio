5c443cff0dafd50ea84a2a5128c124997769c969
Muzammil96313 <muzammil.hassan.dev7@gmail.com>
2025-01-08 14:02:38 +0500

    new

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -7,24 +7,30 @@ const Portfolio = () => {
         <h1 className="text-4xl font-bold mb-8">Portfolio</h1>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
           <div>
-            <img className="rounded-lg" src="computer.webp" alt="" />
+            <img className="rounded-lg" src="blog.webp" alt="" />
             <div className="flex justify-between items-center">
-              <h4 className="text-lg font-semibold">E-Commerce Website</h4>
-              <button className="btn">Preview</button>
+              <h4 className="text-lg font-semibold">Blog App</h4>
+              <a href="https://blog-murex-ten-84.vercel.app/" target="_blank">
+                <button className="btn">Preview</button>
+              </a>
             </div>
           </div>
           <div>
             <img className="rounded-lg" src="computer.webp" alt="" />
             <div className="flex justify-between items-center">
               <h4 className="text-lg font-semibold">E-Commerce Website</h4>
-              <button className="btn">Preview</button>
+              <a href="https://blog-murex-ten-84.vercel.app/" target="_blank">
+                <button className="btn">Preview</button>
+              </a>
             </div>
           </div>
           <div>
             <img className="rounded-lg" src="computer.webp" alt="" />
             <div className="flex justify-between items-center">
               <h4 className="text-lg font-semibold">E-Commerce Website</h4>
-              <button className="btn">Preview</button>
+              <a href="https://blog-murex-ten-84.vercel.app/" target="_blank">
+                <button className="btn">Preview</button>
+              </a>
             </div>
           </div>
         </div>
